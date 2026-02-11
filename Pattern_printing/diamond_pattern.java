443339c153b30169c25ae9b9e08b947d41cbf600
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-02-11 21:12:05 +0530

    added diamond pattern

--- Pattern_printing/diamond_pattern.java
+++ Pattern_printing/diamond_pattern.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
+
 public class diamond_pattern {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter no : ");
         int n = sc.nextInt();
+        int nsp = n - 1, nst = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*" + " ");
+            }
+            nsp--;
+            nst = nst + 2;
+            System.out.println();
 
-        for( int i = 1; i<n; i++ ){
-            for(int j = 1; j< n-i;j++ ){
-                System.out.print(" "+" ");
+        }
+        nsp = 1;
+        nst = (2 * n) - 3;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print(" " + " ");
             }
-            for(int j = 1; j< n-i; j++ ){
-                System.out.print("*"+" ");
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*" + " ");
             }
+            nsp++;
+            nst = nst - 2;
             System.out.println();
         }
     }
