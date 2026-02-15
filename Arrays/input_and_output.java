384ec3ec1005ff17384a2e6e0f83e477f77d6409
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-02-15 20:55:43 +0530

    updated file

--- Arrays/input_and_output.java
+++ Arrays/input_and_output.java
@@ -4,17 +4,22 @@ import java.util.*;
 
 public class input_and_output {
     public static void main(String[] args) {
+        //input line
         Scanner sc = new Scanner(System.in);
+        //initilization o array.
+        int[] arr = { 1, 19, 23, 131, 144, 463, 645, 53, 656, 654645, 575, 4534, 6, 65, 6, 757 };
 
-        int[] arr = new int[10];
+        //initilization through new keyword.
+        // int[] arr = new int[10]; --> means arrar of size 10 
+        
         // input an array.
-        System.out.print("Enter Arrays element : ");
-        for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
-        }
+        // System.out.print("Enter Arrays element : ");
+        // for (int i = 0; i < 10; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
         // output in array.
-        for (int i = 0; i < 10; i++) {
-            System.out.println(arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
 
     }
