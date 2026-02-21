356a2b73063bd4cbc0b689b2d1d4e635c37cd7e7
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-02-21 21:50:39 +0530

    Added minimum of arr pg

---
+++ Arrays/minimum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class minimum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array :");
+        int size = sc.nextInt();
+        //array
+        int[] arr = new int[size];
+        int min = Integer.MAX_VALUE;
+        System.out.print("Enter the elements of Array : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+
+            //calculate minimum.
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.print("Minimum value : "+min);
+    }
+}
