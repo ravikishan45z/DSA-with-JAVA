51b64abb2fa14ee7c07d1b991d10180505f04ecd
RAVIKISHAN GAUR <ravikishan202t@gmail.com>
2026-05-12 21:06:31 +0530

    Strings PG

---
+++ Strings/newstr.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class newstr {
+    public static void main(String[] args) {
+        String s = new String("Ravi");
+        // String s = "Ravi";
+        String t = new String("Ravi");
+        // String t = "Ravi";
+
+        // if (s == t) {
+        //     System.out.println("hello");
+        // } else {
+        //     System.out.println("wrong");
+        // }
+
+        System.out.println(s.equals(t));
+
+    }
+}
